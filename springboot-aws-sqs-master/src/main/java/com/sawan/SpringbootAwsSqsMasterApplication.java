a68f0a42aa13a3dbddd6cfbbd70352fc6c561e44
dhamrendrasawan <dhamrendrasawan@gmail.com>
2022-08-07 15:02:35 +0530

    Add files via upload

---
+++ springboot-aws-sqs-master/src/main/java/com/sawan/SpringbootAwsSqsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.sawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSqsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSqsMasterApplication.class, args);
+	}
+
+}
